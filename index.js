6ce51ac70216704014c069a05c06e5eb2e067cce
MAIKA <maikanguyenn@gmail.com>
2023-02-21 01:28:10 +0100

    v2 twitter feed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
-const url = 'https://whois.fdnd.nl/api/v1/squad/'
+const url = "https://whois.fdnd.nl/api/v1/squad/squat-c-2022";
+const data = await fetch(url).then((response) => response.json()).catch((error) => error);
 
 // Maak een nieuwe express app
 const app = express()
@@ -12,16 +13,9 @@ app.use(express.static('public'))
 
 // Maak een route voor de index
 app.get('/', (request, response) => {
-  console.log(request.query.squad)
-
-  let slug = request.query.squad || 'squad-a-2022'
-  let orderBy = request.query.orderBy || 'name'
-  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
-
-  fetchJson(squadUrl).then((data) => {
-    response.render('index', data)
+  console.log(request.query.squad);
+    response.render('index', data);
   })
-})
 
 // app.get('/members', (request, response) => {
 //   response.send('Joepie!!')
@@ -33,13 +27,3 @@ app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
 
-/**
- * Wraps the fetch api and returns the response body parsed through json
- * @param {*} url the api endpoint to address
- * @returns the json response from the api endpoint
- */
-async function fetchJson(url) {
-  return await fetch(url)
-    .then((response) => response.json())
-    .catch((error) => error)
-}
