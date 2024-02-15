77561fe42741a30440ddd8a069d62aad20758baf
DamianR04 <damian.romeijn@hva.nl>
2024-02-15 11:45:01 +0100

    v0.2
    TTS 0.2
    
    -Filters
    -Squad change

---
+++ public/script.js
@@ -0,0 +1,26 @@
+let dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    let select = dropdown.querySelector('.select');
+    let caret = dropdown.querySelector('.caret');
+    let menu = dropdown.querySelector('.menu');
+    let options = dropdown.querySelectorAll('.menu li');
+    let selected = dropdown.querySelector('.selected');
+
+
+select.addEventListener('click', () => {
+select.classList.toggle('select-clicked');
+caret.classList.toggle('caret-rotate');
+menu.classList.toggle('menu-open');
+});
+
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        option.classList.toggle('active');
+
+        let selectedOptions = Array.from(menu.querySelectorAll('.active')).map(option => option.innerText);
+        selected.innerText = selectedOptions.join(', ');
+    });
+});
+});
+
