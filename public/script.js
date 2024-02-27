d01aaea57f8ba1072350e5f3e2696938f0aec5d0
DamianR04 <damian.romeijn@hva.nl>
2024-02-27 13:49:42 +0100

    V0.7
    Edit header
    Preloader added
    Emoji's working

--- public/script.js
+++ public/script.js
@@ -21,3 +21,28 @@ options.forEach(option => {
 });
 });
 
+let i = 0;
+let stars = document.querySelector('.star1 + .star2 + star3 + star4');
+let aarde = document.querySelector('.aarde');
+let raket = document.querySelector('.rocket');
+let number = document.querySelector('.number');
+let site = document.querySelector('.website');
+let preloader = document.querySelector('.preloader')
+
+
+
+let interval = setInterval(function(){
+    number.innerHTML = i+'<span>%</span>';
+    number.style.height = i+'%';
+    i++
+    if(i == 101){
+        clearInterval(interval)
+    setTimeout(function(){
+        preloader.style.opacity = '0';
+        preloader.style.visibility = 'hidden';
+        site.style.opacity = '1';
+        site.style.visibility = 'visible';
+      
+    })
+    }
+},40)
--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-// Importeer het npm pakket express uit de node_modules map
-import express from 'express'
-
+/*** Express setup & start ***/
 // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
-// Stel het basis endpoint in
-const apiUrl = 'https://fdnd.directus.app/items'
-
-// Haal alle squads uit de WHOIS API op
-const squadData = await fetchJson(apiUrl + '/squad')
+// Importeer het npm pakket express uit de node_modules map
+import express from 'express'
 
 // Maak een nieuwe express app aan
 const app = express()
+
+
 
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
@@ -21,6 +18,28 @@ app.set('views', './views')
 
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: true}))
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+
+/*** Routes & data ***/
+// Haal alle squads uit de WHOIS API op
+
+// Stel het basis endpoint in
+const apiUrl = 'https://fdnd.directus.app/items/'
+
+
+const squadData = await fetchJson(apiUrl + '/squad')
+
 
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
@@ -49,11 +68,56 @@ app.get('/person/:id', function (request, response) {
   })
 })
 
-// Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
 
-// Start express op, haal daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+//Als we vanuir de browser een POST doen op de detailpagina van een persoon
+
+app.post('/detail/:id/SE-GL-emoji', function (request, response) {
+
+  //Stap 1: Haal de huidige gegevens voor deze persoon op, uit de WHOIS API
+
+  fetchJson('https://fdnd.directus.app/items/person/' + request.params.id).then((apiResponse) => {
+  // Het custom field is een String, dus die moeten we eerst omzetten (= parsen)
+  // naar een Object, zodat we er mee kunnen werken
+  try {
+    apiResponse.data.custom = JSON.parse(apiResponse.data.custom)
+  } catch (e) {
+    apiResponse.data.custom = {}
+  }
+  
+  //STAP 2: Voeg de emoji toe aan het custom object
+  
+  if (apiResponse.data.custom.emojis === true || !apiResponse.data.custom.emojis) {
+    apiResponse.data.custom.emojis = {}
+  }
+
+  if (request.body.emoji == 'star-eyes') {
+    if (!apiResponse.data.custom.emojis.starEyes) {
+      apiResponse.data.custom.emojis.starEyes = 0
+    }
+    apiResponse.data.custom.emojis.starEyes++
+  }
+
+  if (request.body.emoji == 'glasses-eyes') {
+    if (!apiResponse.data.custom.emojis.glassesEyes) {
+      apiResponse.data.custom.emojis.glassesEyes = 0
+    }
+    apiResponse.data.custom.emojis.glassesEyes++
+  }
+
+//STAP 3: Overschrijf het custom field voor deze persoon
+
+fetchJson('https://fdnd.directus.app/items/person/' + request.params.id, {
+  method: 'PATCH',
+  body: JSON.stringify({
+    custom: apiResponse.data.custom
+}),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8'
+  }
+}).then((patchResponse) => {
+  //redirect naar de persoon pagina
+  response.redirect(303, '/');
+});
+  }
+);
+});
